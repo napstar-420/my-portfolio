f2894a19122f346facd1c20264b82192c931dfc3
zohaib khan <zohaibkhan1162002@gmail>
2022-09-06 02:35:46 -0700

    Create Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mainGreen': '#56FFC2',
+        'mainBlack': '#303030',
+      },
+      fontFamily: {
+        openSans: ['Open Sans', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
