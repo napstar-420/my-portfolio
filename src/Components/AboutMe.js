1d34bac473aa28c679efb9854cbdea651f8445b4
Zohaib Khan <zohaibkhan1162002@gmail.com>
2022-12-29 23:54:46 +0500

    update portfolio

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,111 +1,60 @@
-import React, {useState, useEffect} from 'react'
-import myPic from '../Images/my_pic.webp';
-import {AiFillHtml5} from 'react-icons/ai';
-import {FaCss3Alt, FaBootstrap, FaGitAlt, FaGithub} from 'react-icons/fa';
-import {IoLogoJavascript} from 'react-icons/io';
-import {SiTailwindcss, SiSass, SiReact, SiRedux, SiVisualstudiocode} from 'react-icons/si';
-import resume from '..//Docs/resume.pdf'
+import React from 'react'
+import myPic from '../Images/my_pic.jpg';
+import {BsTelephone} from 'react-icons/bs';
+import {HiOutlineMail} from 'react-icons/hi';
+import {MdDevices} from 'react-icons/md';
+import {VscRocket} from 'react-icons/vsc';
+import {GoBrowser} from 'react-icons/go';
 
-
-function AboutMe() {
-
-    const secondNames = ['Problem Solver', 'Programmer', 'Web Developer', 'UI / UX Designer', 'Coder']
-    const [index, setIndex] = useState(0);
-
-    useEffect(()=>{
-        let length = secondNames.length;
-        setTimeout(()=>{
-            let n = index;
-            if(n === length - 1){
-                setIndex(0);
-            } else {
-                setIndex(n+1);
-            }
-        },2000)
-
-    },[index])
-
+export default function AboutMe() {
 
     return (
-        <section className="bg-mainGreen w-full min-h-screen p-4" id='about_me'>
-            <div className='bg-mainBlack w-full rounded-2xl p-4 sm:p-8'>
-                <article className=" max-w-5xl mx-auto">
-                    <div className='flex flex-col items-center my-2'>
-                        <div className='flex flex-col md:flex-row items-center w-full'>
-                            <img src={myPic} alt="" className='w-[150px] md:hidden'/>
-                            <div className='text-center md:text-left my-4'>
-                                <h2 className='text-white font-bold font-poppins text-2xl sm:text-4xl'>Who am i</h2>
-                                <h2 className={`text-mainGreen font-bold font-poppins text-2xl sm:text-4xl`}>{secondNames[index]}</h2>
-                            </div>
-                        </div>
-                        <div className='md:flex justify-start items-center md:w-full'>
-                            <img src={myPic} alt="computer_illustration" className='hidden md:block w-[150px] mr-8'/>
-                            <p className='text-white text-center tracking-wider font-quicksand font-light my-4 sm:my-8 md:mr-8 md:text-left max-w-[40em]'>
-                                Hi, My name is Zohaib. I am a self taught Frontend Web Developer based in Lahore, PK. I Build websites in React JS along with many other different technologies. As a developer i love solving problems and i have keen eye for the design as well. Also i enjoy learning new things because in this modern era new technologies are being released everyday. As a new Developer i am looking forward to job opportunities where i can polish my skills. 
-                            </p>
-                        </div>
+        <section className="bg-mainGreen w-screen min-h-screen px-4 grid md:place-items-center py-32 relative" id='about_me'>
+            <div className='bg-mainBlack lg:scale-100 2xl:scale-110 w-full md:w-[700px] md:h-auto h-[calc(100%-6rem)] md:grid grid-cols-[auto_1fr] shadow-md shadow-gray-700  rounded-md relative'>
+                <div className={`w-[150px] md:w-[200px] h-[150px] md:h-auto mx-auto md:mx-0 bg-[url('/src/Images/my_pic.jpg')] bg-cover bg-right md:bg-none rounded-full md:rounded-none relative bottom-20 md:static md:bottom-0`}>
+                    <img src={myPic} alt='' className='w-full hidden md:block'/>
+                </div>
+                <div className='py-8 px-4 md:px-10 relative bottom-24 md:bottom-0 text-center md:text-left w-full'>
+                    <h2 className='text-2xl font-poppins font-bold text-mainGreen mb-1'>About me</h2>
+                    <p className='text-white md:text-sm font-poppins font-thin'>
+                        My name is Zohaib Khan, I am a self taught frontend developer based in Lahore, PK.
+                        I have experience of working on multiple projects. Passionate, hardworking and open to opportunities. My specialty includes building websites in React JS along with many other technologies. As a developer i love solving problems and always eager to learn more.
+                    </p>
+                    <h5 className='flex items-center text-white font-poppins mt-4 mb-2 font-light'>
+                        <BsTelephone className='mr-2 text-mainGreen'/> +92-322-3947372
+                    </h5>
+                    <h5 className='flex items-center text-white font-poppins font-light'>
+                        <HiOutlineMail className='mr-2 text-lg text-mainGreen'/> dev.zohaib.work@gmail.com
+                    </h5>
+                    <h3 className='text-xl font-bold font-poppins text-white mt-8 mb-2 text-left'>Interests</h3>
+                    <div className='flex items-start flex-wrap'>
+                        <h4 className='px-4 py-[2px] font-poppins text-black bg-mainGreen rounded-full mr-2 mb-2'>Music</h4>
+                        <h4  className='px-4 py-[2px] font-poppins text-black bg-mainGreen rounded-full mr-2 mb-2'>Gaming</h4>
+                        <h4  className='px-4 py-[2px] font-poppins text-black bg-mainGreen rounded-full mr-2 mb-2'>Cyber Security</h4>
+                        <h4  className='px-4 py-[2px] font-poppins text-black bg-mainGreen rounded-full mr-2 mb-2'>Art</h4>
                     </div>
-                    <div className='md:grid grid-cols-2 '>
-                        <div className='mt-14 text-center md:text-left'>
-                            <h2 className='text-mainGreen font-medium font-poppins text-2xl'>My Technology Stack</h2>
-                            <div className='text-white my-20 flex flex-col items-center h-[20em] w-full md:w-max md:px-16'>
-                                <div className='stack_item bg-[#e34c26]  rounded-lg grid place-items-center z-[11] overflow-visible'>
-                                    <h3 className='font-poppins text-xl font-semibold'><AiFillHtml5 className='mr-1'/> Html 5</h3>
-                                </div>
-                                <div className='stack_item bg-[#264de4] rounded-lg grid place-items-center z-[10]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><FaCss3Alt className='mr-1'/> Css 3</h3>
-                                </div>
-                                <div className='stack_item bg-[#f0db4f] rounded-lg grid place-items-center z-[9]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><IoLogoJavascript className='mr-1'/> Javascript</h3>
-                                </div>
-                                <div className='stack_item bg-[#7952b3] rounded-lg grid place-items-center z-[8]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><FaBootstrap className='mr-1'/> Bootstrap</h3>
-                                </div>
-                                <div className='stack_item bg-[#0eb5fd]  rounded-lg grid place-items-center z-[7]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><SiTailwindcss className='mr-1'/> Tailwind</h3>
-                                </div>
-                                <div className='stack_item bg-[#cc6699] rounded-lg grid place-items-center z-[6]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><SiSass className='mr-1'/> Sass</h3>
-                                </div>
-                                <div className='stack_item bg-[#61DBFB] rounded-lg grid place-items-center z-[5]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><SiReact className='mr-1'/> React JS</h3>
-                                </div>
-                                <div className='stack_item bg-[#9e7bd6] rounded-lg grid place-items-center z-[4]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><SiRedux className='mr-1'/> Redux JS</h3>
-                                </div>
-                                <div className='stack_item bg-[#0066b8] rounded-lg grid place-items-center z-[3]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><SiVisualstudiocode className='mr-1'/> VS Code</h3>
-                                </div>
-                                <div className='stack_item bg-[#f44d27] rounded-lg grid place-items-center z-[2]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><FaGitAlt className='mr-1'/> Git</h3>
-                                </div>
-                                <div className='stack_item bg-[#474747] rounded-lg grid place-items-center z-[1]'>
-                                    <h3 className='font-poppins text-xl font-semibold'><FaGithub className='mr-1'/> Github</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='my-14 px-4'>
-                            <h2 className='text-mainGreen font-medium font-poppins text-2xl'>Skill set</h2>
-                            <ul className='text-white list-disc px-4 mt-6'>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Responsive Design</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>React Applications</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>DOM Manipulation</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Clean Code</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Building Single Page Applications</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Reuseable Components</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Modern Design</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Working with Api's</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>UI / UX Design</li>
-                                <li className='font-quicksand text-lg sm:text-xl mb-2'>Figma, Photoshop, Premiere</li>
-                            </ul>
-                        </div>
-                        <div className='h-2 w-[50vw] mx-auto bg-mainGreen rounded md:hidden'/>
-                        <h2 className='text-white mt-8 mb-4 px-4 md:px-0 font-poppins sm:text-lg md:text-xl'>Have a look at my <a href={resume} target='_blank' className='px-4 py-1 bg-mainGreen rounded text-mainBlack font-semibold mx-2 hover:bg-[#3dbe8f]' rel="noreferrer">Resume</a></h2>
+                </div>
+            </div>
+            <div className='mt-8 md:mt-32 2xl:scale-110 lg:scale-100 w-full md:w-[700px]'>
+                <h2 className='text-4xl font-poppins font-bold text-center mb-8 w-full'>My Websites are</h2>
+                <div className='grid md:grid-cols-3 gap-4 w-full'>
+                    <div className='bg-mainBlack text-white flex flex-col items-center rounded-md shadow-md shadow-mainBlack p-2 md:p-8 hover:-translate-y-2 hover:scale-105 hover:shadow-lg hover:shadow-green-800 transition-all cursor-pointer'>
+                        <MdDevices className='text-8xl mb-4 text-mainGreen'/>
+                        <h3 className='text-xl font-semibold font-poppins mb-4'>Responsive</h3>
+                        <p className='text-sm font-poppins font-thin text-center'>My layouts work on all devices desktop, mobile or tablet</p>
                     </div>
-                </article>
+                    <div className='bg-mainBlack text-white flex flex-col items-center rounded-md shadow-md shadow-mainBlack p-8 hover:-translate-y-2 hover:scale-105 hover:shadow-lg hover:shadow-green-800 transition-all cursor-pointer'>
+                        <VscRocket className='text-8xl mb-4 text-mainGreen'/>
+                        <h3 className='text-xl font-semibold font-poppins mb-4'>Fast</h3>
+                        <p className='text-sm font-poppins font-thin text-center'>My layouts work on all devices desktop, mobile or tablet</p>
+                    </div>
+                    <div className='bg-mainBlack text-white flex flex-col items-center rounded-md shadow-md shadow-mainBlack p-8 hover:-translate-y-2 hover:scale-105 hover:shadow-lg hover:shadow-green-800 transition-all cursor-pointer'>
+                        <GoBrowser className='text-8xl mb-4 text-mainGreen'/>
+                        <h3 className='text-xl font-semibold font-poppins mb-4 text-center'>Cross Browser Compatible</h3>
+                        <p className='text-sm font-poppins font-thin text-center'>My layouts work on all devices desktop, mobile or tablet</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
 }
-
-export default AboutMe;
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -4,6 +4,10 @@ import goodwell_outlet from "../Images/goodwell_outlet.webp";
 import geo_weather from "../Images/geo_weather.webp";
 import random_quote_site from "../Images/random_quote_site.webp";
 import spicella_spanish_kitchen from "../Images/spicella_spanish_kitchen.webp";
+import {AiFillHtml5} from 'react-icons/ai';
+import {FaCss3Alt, FaBootstrap, FaGitAlt, FaGithub} from 'react-icons/fa';
+import {IoLogoJavascript} from 'react-icons/io';
+import {SiTailwindcss, SiSass, SiReact, SiRedux, SiVisualstudiocode, SiReactrouter} from 'react-icons/si';
 
 const Work = () => {
   const projects = [
@@ -43,44 +47,106 @@ const Work = () => {
       codeURL: "https://github.com/napstar-420/spicella-spanish-kitchen",
     },
   ];
+
   return (
-    <section className="py-16" id="work">
-      <h2 className="text-mainGreen font-poppins font-semibold text-center text-2xl sm:text-3xl px-4">
+    <section className='py-32' id='work'>
+      <div className='mx-auto max-w-[700px] mb-32 px-4'>
+        <h1 className='text-mainGreen font-poppins font-semibold text-center text-2xl sm:text-3xl px-4 mb-8'>
+          My Technology Stack
+        </h1>
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(150px,1fr))] gap-4'>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <AiFillHtml5 className='text-orange-500 text-8xl mb-1' />
+            HTML
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <FaCss3Alt className='text-blue-500 text-8xl mb-1' />
+            CSS
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <IoLogoJavascript className='text-yellow-500 text-8xl mb-1' />
+            Javascript
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiReact className='text-blue-500 text-8xl mb-1' />
+            React
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiRedux className='text-purple-700 text-8xl mb-1' />
+            Redux
+          </div>
+          <div className='text-2xl text-white text-center font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiReactrouter className='text-red-700 text-8xl mb-1' />
+            React Router
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <FaBootstrap className='text-[#6e10ee] text-8xl mb-1' />
+            Bootstrap
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiTailwindcss className='text-blue-600 text-8xl mb-1' />
+            Tailwind
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiSass className='text-pink-600 text-8xl mb-1' />
+            Sass
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <FaGitAlt className='text-orange-600 text-8xl mb-1' />
+            Git
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <FaGithub className='text-black text-8xl mb-1' />
+            Git
+          </div>
+          <div className='text-2xl text-white font-poppins font-medium flex flex-col items-center justify-between p-4 shadow shadow-[#202020] rounded transition hover:shadow-mainGreen'>
+            <SiVisualstudiocode className='text-blue-500 text-8xl mb-1' />
+            VsCode
+          </div>
+        </div>
+      </div>
+      <h2 className='text-mainGreen font-poppins font-semibold text-center text-2xl sm:text-3xl px-4'>
         Some of my Projects
       </h2>
-      <p className="text-white font-openSans text-center text-xs my-4 md:my-8 sm:text-sm max-w-[35em] mx-auto opacity-50 px-4">
+      <p className='text-white font-openSans text-center text-xs my-4 md:my-8 sm:text-sm max-w-[35em] mx-auto opacity-50 px-4'>
         Here is some of my work which i made while learning frontend
         development. They are not the best of me but still i had a lot of fun
         while making these projects. Some new interesting projects are coming
         soon.
       </p>
-      <article className="mt-14">
+      <article className='mt-14'>
         {projects.map((project, index) => {
           const { name, img, desc, hostedURL, codeURL } = project;
           return (
-            <div className="w-full mx-auto bg-[#202020] my-10">
-              <div className="w-[90%] lg:max-w-5xl py-8 mx-auto">
-                <h2 className="text-mainGreen text-2xl font-semibold font-poppins tracking-widest">
+            <div key={index} className='w-full mx-auto bg-[#202020] my-10'>
+              <div className='w-[90%] lg:max-w-5xl py-8 mx-auto'>
+                <h2 className='text-mainGreen text-2xl font-semibold font-poppins tracking-widest'>
                   {name}
                 </h2>
-                <div className="md:flex justify-between items-center">
-                    <p className="text-white font-quicksand my-4 text-sm sm:text-base max-w-[20em] lg:leading-7">{desc}</p>
-                    <img src={img} alt={name} className="w-[300px] sm:w-[400px] mx-auto md:mx-0 lg:scale-125 lg:hover:scale-150 transition-all cursor-crosshair"/>
+                <div className='md:flex justify-between items-center'>
+                  <p className='text-white font-quicksand my-4 text-sm sm:text-base max-w-[20em] lg:leading-7'>
+                    {desc}
+                  </p>
+                  <img
+                    src={img}
+                    alt={name}
+                    className='w-[300px] sm:w-[400px] mx-auto md:mx-0 lg:scale-125 lg:hover:scale-150 transition-all cursor-crosshair'
+                  />
                 </div>
-                <div className="mt-4 flex flex-wrap">
+                <div className='mt-4 flex flex-wrap'>
                   <a
                     href={hostedURL}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="font-poppins text-sm md:text-base font-semibold text-mainBlack bg-mainGreen px-5 py-1 mr-1 border-[3px] border-mainGreen hover:bg-[#4ce6ad]"
+                    target='_blank'
+                    rel='noreferrer'
+                    className='font-poppins text-sm md:text-base font-semibold text-mainBlack bg-mainGreen px-5 py-1 mr-1 border-[3px] border-mainGreen hover:bg-[#4ce6ad]'
                   >
                     Live Preview
                   </a>
                   <a
                     href={codeURL}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="font-poppins text-sm md:text-base font-semibold text-mainGreen bg-transparent hover:bg-[#353535] px-5 py-1 ml-1 border-[3px] border-mainGreen"
+                    target='_blank'
+                    rel='noreferrer'
+                    className='font-poppins text-sm md:text-base font-semibold text-mainGreen bg-transparent hover:bg-[#353535] px-5 py-1 ml-1 border-[3px] border-mainGreen'
                   >
                     Source Code
                   </a>
@@ -89,7 +155,9 @@ const Work = () => {
             </div>
           );
         })}
-        <p className="text-center text-white font-openSans">New Projects Coming Soon</p>
+        <p className='text-center text-white font-openSans'>
+          New Projects Coming Soon
+        </p>
       </article>
     </section>
   );
